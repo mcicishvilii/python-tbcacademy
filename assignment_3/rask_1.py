52e57cb0876610d34c403e7cb4a05c0d47472829
mcicishvilii <mcicishvilii@gmail.com>
2025-03-14 20:11:40 +0400

    first task done

--- assignment_3/rask_1.py
+++ assignment_3/rask_1.py
@@ -1,4 +1,8 @@
-true_or_false = input("true or false?: ").lower()
+import math
 
-if true_or_false == "true":
-    print("whoala")
+number_to_power = int(input("please enter the number: "))
+power = int(input("please enter the power: "))
+
+powered_number = math.pow(number_to_power,power)
+
+print(f"the the value of {number_to_power} raised to the power of {power} is: {int(powered_number)}")
