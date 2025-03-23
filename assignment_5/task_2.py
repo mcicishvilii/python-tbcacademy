402b45acd8a3e63e68bdeafdb7618c1317f4fb00
mcicishvilii <mcicishvilii@gmail.com>
2025-03-23 12:43:26 +0400

    task 3 done

--- assignment_5/task_2.py
+++ assignment_5/task_2.py
@@ -2,6 +2,6 @@ for i in range(1, 10):
     line = ""
     j = 1
     while j <= i:
-        line += f"{j} * {i} = {j*i}     "
+        line += f"{j} * {i} = {j * i}     "
         j += 1
     print(line)
