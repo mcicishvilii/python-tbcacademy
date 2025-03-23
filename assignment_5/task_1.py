c0099456fa5aa2726c1328e58d6d3e22da0ed7bf
mcicishvilii <mcicishvilii@gmail.com>
2025-03-23 11:22:30 +0400

    task one done

--- assignment_5/task_1.py
+++ assignment_5/task_1.py
@@ -1,14 +1,13 @@
 input_number = int(input("please enter the whole number from 1 to 50: "))
 temp_number = 0
-divisors = []
 
 if input_number < 1 or input_number > 50:
     print("please enter number from 1 to 50")
 else:
     while temp_number < input_number:
+        divisors = []
         temp_number += 1
-        for i in range (1,temp_number + 1):
+        for i in range(1, temp_number + 1):
             if temp_number % i == 0:
                 divisors.append(i)
-            print(divisors)
-                
+    print(divisors[:3])
