bc746b60ba361a2313e5ee9ddf3ed834da0ab9bf
mcicishvilii <mcicishvilii@gmail.com>
2025-03-27 23:29:52 +0400

    first task done

---
+++ assignment_6/task_1.py
@@ -0,0 +1,19 @@
+import random
+
+attempts = 0
+random_number = random.randint(1, 100)
+
+while attempts < 10:
+    input_number = int(input("enter number: "))
+    
+    if input_number > random_number:
+        print("high")
+    elif input_number < random_number:
+        print("low")
+    else:
+        print("gilocav")
+        break
+    attempts += 1
+    if(attempts == 10):
+        print("you lost :(")
+        break
